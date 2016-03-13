cd8b1d7e5f8838e44f1b16a5fa8ef5e62778573e
Mitch <mitch.lindsay@tutanota.com>
2016-03-13 13:19:48 -0400

    Changes icon font to svg

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var browserSync = require('browser-sync').create();
 var exec = require('child_process').exec;
 var gulp = require('gulp');
+var path = require('path');
 var shell = require('gulp-shell');
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
 
 gulp.task('clean', shell.task(['bundle exec jekyll clean']));
 
@@ -14,6 +17,24 @@ gulp.task('doctor', shell.task(['bundle exec jekyll doctor']));
 gulp.task('serve', function () {
   browserSync.init({server: {baseDir: 'static/'}});
   gulp.watch('static/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('svgstore', function () {
+  return gulp
+    .src('source/_assets/images/map/*.svg')
+    .pipe(svgmin(function (file) {
+      var prefix = path.basename(file.relative, path.extname(file.relative));
+      return {
+        plugins: [{
+          cleanupIDs: {
+            prefix: prefix + '-',
+            minify: true
+          }
+        }]
+      }
+    }))
+    .pipe(svgstore())
+    .pipe(gulp.dest('source/_assets/images/icons'));
 });
 
 gulp.task('publish', ['clean', 'build'], function() {
