7e3935d9e7405729c90289da5b31addc7d5f5d6e
Mitch <mitch.lindsay@tutanota.com>
2016-03-14 22:07:56 -0400

    Adds font awesome

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "https://rubygems.org"
 gem 'github-pages'
 gem 'jekyll-assets'
 gem 'sass'
+gem 'font-awesome-sass'
 gem 'uglifier'
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var browserSync = require('browser-sync').create();
 var exec = require('child_process').exec;
 var gulp = require('gulp');
 var shell = require('gulp-shell');
-var svgmin = require('gulp-svgmin');
 
 gulp.task('clean', shell.task(['bundle exec jekyll clean']));
 
@@ -15,16 +14,6 @@ gulp.task('doctor', shell.task(['bundle exec jekyll doctor']));
 gulp.task('serve', function () {
   browserSync.init({server: {baseDir: 'static/'}});
   gulp.watch('static/**/*.*').on('change', browserSync.reload);
-});
-
-gulp.task('svgmin', function () {
-  return gulp.src('_assets/images/icons/*.svg')
-    .pipe(svgmin({
-      js2svg: {
-        pretty: true
-      }
-    }))
-    .pipe(gulp.dest('_assets/images/icons'));
 });
 
 gulp.task('publish', ['clean', 'build'], function() {
