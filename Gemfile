e99dbaa6a414734fb6c421a54c86b6cf86f31fc7
Mitch <work@mitchlindsay.me>
2017-05-28 18:05:12 -0400

    Simplify build process. Added header/footer

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 
+gem 'bootstrap-sass', '3.3.7'
 gem 'bundler', '1.13.7'
+gem 'font-awesome-sass', '4.7.0'
 gem 'jekyll', '3.3.1'
 gem 'jekyll-assets', '2.2.8'
 gem 'sass', '3.4.23'
--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,6 @@ gulp.task('clean', shell.task([
   'bundle exec jekyll clean'
 ]));
 
-gulp.task('copy', function() {
-  gulp.src('./node_modules/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest('./source/_assets/js/'));
-  gulp.src('./node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js')
-    .pipe(gulp.dest('./source/_assets/js/'));
-  gulp.src('./node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
-    .pipe(gulp.dest('./source/_assets/scss/'));
-  gulp.src('./node_modules/bootstrap-sass/assets/stylesheets/bootstrap/**')
-    .pipe(gulp.dest('./source/_assets/scss/bootstrap/'));
-});
-
 gulp.task('build-prod', shell.task(['JEKYLL_ENV=production bundle exec jekyll build']));
 gulp.task('build-dev', shell.task(['JEKYLL_ENV=development bundle exec jekyll build --watch']));
 gulp.task('doctor', shell.task(['bundle exec jekyll doctor']));
@@ -29,7 +18,5 @@ gulp.task('serve', function () {
   gulp.watch('static/**/*.*').on('change', browserSync.reload);
 });
 
-/* Use this for production builds */
 gulp.task('publish', ['clean', 'build-prod'], function() { exec('rake publish'); });
-/* Use this for development builds & testing */
 gulp.task('default', ['clean', 'build-dev', 'serve']);
